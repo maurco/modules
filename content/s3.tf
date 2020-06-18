f10cb18865f8012666893e79390ba16865824f9b
Jason Maurer <jason@maur.co>
2020-06-17 23:40:04 -0600

    Remove quotes from depends_on argument

--- content/s3.tf
+++ content/s3.tf
@@ -57,5 +57,5 @@ resource "aws_s3_bucket_public_access_block" "main" {
 resource "aws_s3_bucket_policy" "main" {
   bucket     = aws_s3_bucket.main.id
   policy     = data.aws_iam_policy_document.main.json
-  depends_on = ["aws_s3_bucket_public_access_block.main"]
+  depends_on = [aws_s3_bucket_public_access_block.main]
 }
